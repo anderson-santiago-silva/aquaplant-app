d7c47a50347bc807fa4194f1dc8276c0744f5eb9
anderson-santiago-silva <uusantiago@gmail.com>
2021-06-07 15:27:03 -0300

    Implemented payment with paypal

---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,167 @@
+import Axios from 'axios';
+import { PayPalButton } from 'react-paypal-button-v2'; 
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { detailsOrder } from '../actions/orderActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+export default function OrderScreen(props) {
+  const orderId = props.match.params.id;
+  const [sdkReady, setSdkReady] = useState(false);
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const addPaypalScript = async () => {
+      const { data } = await Axios.get('/api/config/paypal');
+      const script = document.createElement('script');
+      script.type = "text/javascript";
+      script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
+      script.async = true;
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      document.body.appendChild(script);
+    };
+    if (!order) {
+      dispatch(detailsOrder(orderId));
+    } else {
+      if (!order.isPaid) {
+        if (!window.paypal) {
+          addPaypalScript();
+        } else {
+          setSdkReady(true);
+        }
+      }
+    }
+  }, [dispatch, order, orderId, sdkReady]);
+
+  const successPaymentHandler = () => {
+    // TODO: dispatch pay order
+  };
+
+  return loading ? (
+    <LoadingBox></LoadingBox>
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
+    <div>
+      <h1>Pedido {order._id}</h1>
+      <div className="row top">
+        <div className="col-2">
+          <ul>
+            <li>
+              <div className="card card-body">
+                <h2>Dados para envio</h2>
+                <p>
+                  <strong>Nome:</strong> {order.shippingAddress.fullName} <br/>
+                  <strong>Endereço:</strong> {order.shippingAddress.address},
+                  {order.shippingAddress.state}, {order.shippingAddress.city},
+                  {order.shippingAddress.postalCode}, {order.shippingAddress.country}                  
+                </p>
+                {order.isDelivered ? ( 
+                <MessageBox variant="success">
+                  Entregue em {order.deliveredAt}
+                </MessageBox>
+                ) : (
+                <MessageBox variant="danger">Não entregue</MessageBox>
+                )}
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Pagamento</h2>
+                <p>
+                  <strong>Método:</strong> {order.paymentMethod}
+                </p>
+                {order.isPaid ? ( 
+                <MessageBox variant="success">
+                  Pago em {order.paidAt}
+                </MessageBox>
+                ) : (
+                <MessageBox variant="danger">Pagamento não efetuado</MessageBox>
+                )}
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Resumo</h2>
+                <ul>
+              {order.orderItems.map((item) => (
+                  <li className="row">
+                    <div>
+                      <img 
+                        src={item.image} 
+                        alt={item.name} 
+                        className="small"
+                      />
+                    </div>
+                    <div className="min-30">
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </div>
+                    <div>
+                      {item.qty} x R$ {item.price} = R$ {item.qty * item.price}
+                    </div>
+                  </li>
+                ))}
+            </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <h2>Resumo do pedido</h2>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Itens</div>
+                  <div>R$ {order.itemsPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                <div>Entrega</div>
+                  <div>R$ {order.shippingPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                <div>Tax</div>
+                  <div>R$ {order.taxPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>
+                    <strong>Total pedido</strong>
+                  </div>
+                  <div>
+                    <strong>R$ {order.totalPrice.toFixed(2)}</strong>
+                  </div>
+                </div>
+              </li>
+              {
+                !order.isPaid && (
+                  <li>
+                    {!sdkReady ? (
+                      <LoadingBox></LoadingBox>
+                    ) : (
+                      <PayPalButton
+                        amount={order.totalPrice}
+                        onSuccess={successPaymentHandler}
+                      ></PayPalButton>
+                    )}
+                  </li>
+                )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
