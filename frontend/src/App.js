297010b9610cb65a050c77172e00d21903f560c7
anderson-santiago-silva <uusantiago@gmail.com>
2021-06-08 15:49:37 -0300

    Edit/upload products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import PrivateRoute from './components/PrivateRoute';
 import ProductListScreen from './screens/ProductListScreen';
 import AdminRoute from './components/AdminRoute';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -90,7 +91,12 @@ function App() {
         </header>
         <main>
           <Route path="/cart/:id" component={CartScreen}></Route> 
-          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route 
+            path="/product/:id/edit" 
+            component={ProductEditScreen} 
+            exact
+          ></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
