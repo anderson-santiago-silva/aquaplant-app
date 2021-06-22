cc70bb379b1ec1e99d2b7bb183cc4cc23f508943
anderson-santiago-silva <uusantiago@gmail.com>
2021-06-22 17:17:47 -0300

    publish heroku 22.06

--- backend/config/mongodb.config.js
+++ backend/config/mongodb.config.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 .then(() => console.log('Successfully connect to the Database' ))
 .catch(error => console.log(error)); 
 
--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/aquaplant', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/aquaplant', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
